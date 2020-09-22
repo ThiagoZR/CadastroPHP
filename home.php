ef384704aefe2bfdff628ce6f8c0041397d705a4
Thiago Zacarias Rufino <thiago_rufino@live.com>
2020-09-22 17:16:40 -0300

    First commit

---
+++ home.php
@@ -0,0 +1,44 @@
+<? require_once "validador_acesso.php" ?>
+
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <style>
+      .card-home {
+        padding: 30px 0 0 0;
+        width: 100%;
+        margin: 0 auto;
+      }
+    </style>
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        Bem-Vindo!
+      </a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="logoff.php">SAIR</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="container">    
+      <div class="row">
+
+        <div class="card-home">
+          <div class="card">
+            <div class="card-header">
+              Home
+            </div>
+          </div>
+        </div>
+    </div>
+  </body>
+</html>
