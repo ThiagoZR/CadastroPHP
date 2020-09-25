e9c0814a3e958c2c308e3619f809f894eadcc5f4
Thiago Zacarias Rufino <thiago_rufino@live.com>
2020-09-25 16:07:52 -0300

    Add valida_cadastro.php

---
+++ valida_cadastro.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo '<pre>';
+    echo  $_POST['nome'];
+    echo '<br>';
+    echo $_POST['email'];
+    echo '<br>';
+    echo $_POST['senha'];
+    echo '<br>';
+    echo '</pre>';
+
+?>
